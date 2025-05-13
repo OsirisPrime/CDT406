3d689f4e1efae2fd953b827748ba54f27753142d
kim <ksg22001@student.mdu.se>
2025-05-13 11:14:25 +0200

    Add function to filter subfolders in processed data directory
    Introduce `get_filtered_data_dir` to list subfolders while allowing exclusion of specified folders. This enhances flexibility in managing processed data directories. Updated imports to include `Iterable` for type annotations.

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -1,6 +1,6 @@
 # src/utils/path_utils.py
 from pathlib import Path
-
+from typing import Iterable
 
 def get_project_root() -> Path:
     """Return the path to the project root directory."""
@@ -27,6 +27,16 @@ def get_processed_fast_data_dir() -> Path:
     """Return the path to the processed fast data directory."""
     return get_project_root() / "data" / "processed" / "relabeled_fast_dataset"
 
+def get_filtered_data_dir(ignore_folders: Iterable[str] = ()) -> list[Path]:
+    """
+    Return a list of subfolders in the processed data directory,
+    optionally ignoring multiple specific subfolders.
+    input: list_processed_subfolders(ignore_folders=["subject_05", "temp"])
+    """
+    base_dir = get_processed_data_dir()
+    ignore_set = set(ignore_folders)
+    return [folder for folder in base_dir.iterdir()
+            if folder.is_dir() and folder.name not in ignore_set]
 
 def get_logs_dir() -> Path:
     """Return the path to the logs directory."""
