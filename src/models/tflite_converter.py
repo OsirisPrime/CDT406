48c0bbeef19aecb6fff94316eca474f24af718ea
kim <ksg22001@student.mdu.se>
2025-05-14 19:37:55 +0200

    Add custom STFT layer support and update model selection
    Introduced a custom STFT layer for handling spectrogram generation, enabling its use in the model conversion process. Adjusted model selection logic to prioritize specific indices and extended recursive search in the models directory. Minor comment and print statement updates to improve clarity.

--- src/models/tflite_converter.py
+++ src/models/tflite_converter.py
@@ -3,24 +3,29 @@ import glob
 import tensorflow as tf
 from src.utils.path_utils import get_models_dir
 
-"""
-    Converts the most recently modified trained Keras model (.keras) to a TFLite model.
-    Saves it as `model.tflite` in the current working directory.
-    You can then copy this file to the BeagleBone Green.
-    (.h5) files are a legacy format that are not supported by BeagleBone Green.
-"""
+# Define the custom STFT layer
+class stft_layer(tf.keras.layers.Layer):
+    def __init__(self, frame_length, frame_step):
+        super(stft_layer, self).__init__()
+        self.frame_length = frame_length
+        self.frame_step = frame_step
+
+    def call(self, x):
+        stft = tf.signal.stft(x, frame_length=self.frame_length, frame_step=self.frame_step)
+        spectrogram = tf.abs(stft)
+        return spectrogram
 
 # Get list of all .keras models in the models directory
-model_paths = sorted(glob.glob(os.path.join(str(get_models_dir()), "**/*.keras"), recursive=False))
+model_paths = sorted(glob.glob(os.path.join(str(get_models_dir()), "**/*.keras"), recursive=True))
 
 # Make sure there is at least one model
 if not model_paths:
     raise FileNotFoundError("No .keras model found in models directory.")
 
 # Load the first model
-trained_model = model_paths[0]
+trained_model = model_paths[8] # 0-2: LSTM_STFT_Dense, 3-5: LSTM_STFT, 6-8: LSTM
 print(f"Converting model: {trained_model}")
-model = tf.keras.models.load_model(trained_model)
+model = tf.keras.models.load_model(trained_model, custom_objects={'stft_layer': stft_layer})
 
 # Convert to TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
@@ -30,4 +35,5 @@ tflite_model = converter.convert()
 with open("model.tflite", "wb") as f:
     f.write(tflite_model)
 
+# Saves in the same directory as this file
 print("TFLite model saved as model.tflite")
